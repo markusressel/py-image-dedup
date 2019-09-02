115a4b352e83225af3b3374b477a057311b4a8fb
Markus Ressel <mail@markusressel.de>
2019-09-02 16:18:14 +0200

    refactoring of get_exif_data for better readability fixes for relative file paths

--- py_image_dedup/util/image.py
+++ py_image_dedup/util/image.py
@@ -9,17 +9,21 @@ def get_exif_data(image_file_path: str) -> {}:
     :return: dictionary containing all available exif data entries and their values
     """
 
+    result = {}
     try:
         img = Image.open(image_file_path)
 
         exif_data = img._getexif()
-        if exif_data:
-            return {
-                PIL.ExifTags.TAGS[key]: value for key, value in exif_data.items() if key in PIL.ExifTags.TAGS
-            }
+        if not exif_data:
+            return result
+
+        for k, v in exif_data.items():
+            if k in PIL.ExifTags.TAGS:
+                tag_name = PIL.ExifTags.TAGS[k]
+                result[tag_name] = v
     except Exception as e:
         pass
-    return {}
+    return result
 
 
 def get_pixel_count(image_file_path: str) -> int:
